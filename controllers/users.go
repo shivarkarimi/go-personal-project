59dea148aa64a80d84a58b56f7d7bce6ba154873
Shiva <shiva.razzaghkarimi@foundry.com>
2021-06-29 20:49:53 +0100

    fixed compile error

--- controllers/users.go
+++ controllers/users.go
@@ -9,7 +9,7 @@ type userController struct {
 	userIDPattern *regexp.Regexp
 }
 
-func (uc userController) ServeHTTP(w http.ResponseWriter, r **http.Request) {
+func (uc userController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from user controller!"))
 }
 
